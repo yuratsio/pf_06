e6aaa4eb85b5f2e9d752750bb19ed76ea360c789
yuratsio <93855185+yuratsio@users.noreply.github.com>
2022-05-18 20:25:05 +0900

    Create swiper.js

---
+++ js/swiper.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1.5,
+  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    }
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
